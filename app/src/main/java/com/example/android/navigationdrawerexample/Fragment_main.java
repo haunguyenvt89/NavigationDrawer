3e590d28be863aac3bd287e5f0c216a817963813
Nguyen Tan Hau <haunguyenvt89@gmail.com>
2015-03-27 10:24:36 +0700

    test commit from android studio

---
+++ app/src/main/java/com/example/android/navigationdrawerexample/Fragment_main.java
@@ -0,0 +1,9 @@
+package com.example.android.navigationdrawerexample;
+
+/**
+ * Created by SnsPlusVN_05 on 3/27/2015.
+ */
+public class Fragment_main {
+
+    private final static String TEST = "Test Thanh Cong Git for Android Studio";
+}
